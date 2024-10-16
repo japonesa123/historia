f5524c1e621b621f44e31baf41374709042132a8
japonesa123 <125603284+japonesa123@users.noreply.github.com>
2024-10-16 16:39:28 -0300

    16.10

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -1,5 +1,20 @@
 const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais'
 
 async function vizualizarinformacoesGlobais() {
-    const res =
+    const res = await fetch(url)
+    const dados = await res.json()
+    const pessoasConectadas = (dados.total_pessoas_mundo / 1e9)
+    constpessoasNoMundo = (dados.total_pessoas_mundo / 1e9)
+    const horas =pareInt(dados.tempo_medio)
+    const minutos = Math.round((dados.tempo_medio - horas) * 100)
+    const poecentagemConectada = ((pessoasConectadas / pessoasNoMundo) * 100).toFixed
+
+    const paragrafo = document.createElement('p')
+    paragrafo.classList.add('graficos-container_texto')
+    paragrafo.innerHTML= 'Voce sabia que o mundo tem <span>${pessoasnomundo} bilhoes'
+
+    const container = document.getElementById('graficos-container')
+    container.appendChild(paragrafo)
 }
+
+vizualizarinformacoesGlobais
